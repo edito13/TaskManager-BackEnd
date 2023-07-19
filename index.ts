48acf2dc141ba0ff1f780681bffcf8eba29dd891
edito13 <editotchocosso13@gmail.com>
2023-07-19 08:29:20 +0100

    Initializing the server

--- index.ts
+++ index.ts
@@ -1,16 +1,24 @@
-import express, { Express, Request, Response } from "express";
+import express from "express";
+import cors from "cors";
 import dotenv from "dotenv";
 
 dotenv.config();
 
-const app: Express = express();
-const port = process.env.PORT;
-const PORT = 8000;
+const app = express();
+const router = express.Router();
+const defaultPort = process.env.PORT;
+const PORT = defaultPort || 8000;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+// Permitir requisições com JSON e ev
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Servidor OK!");
 });
 
-app.listen(port || PORT, () => {
-  console.log(`⚡️ Servidor está rodando na porta: http://localhost:${PORT}`);
+app.use(router);
+
+app.listen(PORT, () => {
+  console.log(`⚡️ Servidor está rodando na porta: ${PORT}`);
 });
