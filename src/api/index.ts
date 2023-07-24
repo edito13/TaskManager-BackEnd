424ad373e47f7fe947510f90180b67bd3cf5e9fd
edito13 <editotchocosso13@gmail.com>
2023-07-24 02:04:42 +0100

    Making some adjusts

--- src/api/index.ts
+++ src/api/index.ts
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.get("/", (req, res) => res.send("Servidor OK!"));
 
-require("../routes/tasks")(app);
+require("../controllers/task.controller")(app);
 require("../controllers/user.controller")(app);
 
 app.use(router);
